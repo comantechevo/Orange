ee2c8acd59d0a55345604897e7601077bc5db438
Ruan Carlos <ruancarlosmeira@gmail.com>
2020-01-07 14:31:56 -0200

    mudanças no display

--- atualiza.py
+++ atualiza.py
@@ -2,6 +2,7 @@ import os
 
 os.system('cd Orange/ && git pull origin master')
 os.system('rm /root/Desktop/EVO_CENTRAL_ORANGE_PI.py')
+os.system('rm /root/Desktop/EVO_CENTRAL_ORANGE_PI_1.py')
 os.system('cp -r /root/Desktop/Orange/EVO_CENTRAL_ORANGE_PI.py /root/Desktop')
 os.system('cp -r /root/Desktop/Orange/corrige.py /root/Desktop')
 os.system('cp -r /root/Desktop/Orange/atualiza.py /root/Desktop')
