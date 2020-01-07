cf59659a03b275e6842e151c4405f2d0f39f2971
comantechevo <59568110+comantechevo@users.noreply.github.com>
2020-01-07 11:57:09 -0300

    Add files via upload

---
+++ atualiza.py
@@ -0,0 +1,8 @@
+os.system('cd Orange/ && git pull origin master')
+os.system('rm /root/Desktop/EVO_CENTRAL_ORANGE_PI.py')
+os.system('cp -r /root/Desktop/Orange/EVO_CENTRAL_ORANGE_PI.py /root/Desktop')
+os.system('cp -r /root/Desktop/Orange/corrige.py /root/Desktop')
+os.system('cp -r /root/Desktop/Orange/atualiza.py /root/Desktop')
+print('atualizado')
+os.system('python3 corrige.py')
+print('corrigido!')
